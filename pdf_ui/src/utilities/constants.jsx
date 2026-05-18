6662abd96ea7ee28d70aa8f916b4ab2e90cab3a0
Judah Silva <jsilva@hartnell.edu>
2026-05-18 12:53:24 -0700

    Added Cognito authentication support

--- pdf_ui/src/utilities/constants.jsx
+++ pdf_ui/src/utilities/constants.jsx
@@ -81,19 +81,9 @@ export const validateFormatBucket = (format) => {
   };
 };
 
-// export const DomainPrefix = import.meta.env.REACT_APP_DOMAIN_PREFIX;
-export const HostedUIUrl = import.meta.env.REACT_APP_HOSTED_UI_URL;
-// export const IndentityPoolId = process.env.REACT_APP_IDENTITY_POOL_ID;
-
-// export const FirstSignInAPI = process.env.REACT_APP_UPDATE_FIRST_SIGN_IN;
-// export const CheckAndIncrementQuota = process.env.REACT_APP_UPLOAD_QUOTA_API;
-
-// export const UserPoolClientId = process.env.REACT_APP_USER_POOL_CLIENT_ID;
-// export const UserPoolId = process.env.REACT_APP_USER_POOL_ID;
-
-
-
-
+export const COGNITO_DOMAIN=$REACT_APP_COGNITO_DOMAIN
+export const COGNITO_CLIENT_ID=$REACT_APP_COGNITO_CLIENT_ID
+export const REDIRECT_URI=$REACT_APP_REDIRECT_URI
 
 // Preferably not use
 // export const HostedUserPoolDomain = process.env.REACT_APP_USER_POOL_DOMAIN;
